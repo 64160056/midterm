9c02a242d9e97295ee67649fc66fc658a062c153
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-22 00:20:00 +0700

    Fin:Problem2

--- src/views/SolveProblem2.vue
+++ src/views/SolveProblem2.vue
@@ -1,15 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+const num = ref<number>(0);
+</script>
 
 <template>
   <v-container>
     <v-row>
       <v-text-field
+        v-model.number="num"
         label="Please input Number"
+        class="font-weight-bold"
+        type="input"
         :rules="[(v) => !!v || 'Number is not empty!!']"
       ></v-text-field>
     </v-row>
     <v-row>
-      <v-col></v-col>
+      <v-col v-for="n in num" :key="n" cols="1">
+        {{ n }}
+      </v-col>
     </v-row>
   </v-container>
 </template>
