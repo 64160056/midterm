698f48ca328afecae351b3410b4eccc624a62d48
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-21 19:25:44 +0700

    Show img

--- src/views/SolveProblem1.vue
+++ src/views/SolveProblem1.vue
@@ -1,6 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import angry from "../../feeling_imgs/angry.png";
+const result = ref("");
+</script>
+
 <template>
-  <div></div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-img :src="angry"></v-img>
+      </v-col>
+      <v-col> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped></style>
