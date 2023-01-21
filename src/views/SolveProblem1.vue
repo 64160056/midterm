f344b39f6db56831ca942997eba3a006c81f468d
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-21 19:59:03 +0700

    Fin:Problem1

--- src/views/SolveProblem1.vue
+++ src/views/SolveProblem1.vue
@@ -16,12 +16,12 @@ const OnClick = (name: string) => {
   <v-container>
     <v-row>
       <v-col>
-        <v-img :src="angry" v-if="result === 'angry'"></v-img>
-        <v-img :src="cute"></v-img>
-        <v-img :src="excited"></v-img>
-        <v-img :src="pain"></v-img>
-        <v-img :src="shocked"></v-img>
-        <v-img :src="smile"></v-img>
+        <v-img :src="angry" v-if="result === 'ANGRY'"></v-img>
+        <v-img :src="cute" v-if="result === 'CUTE'"></v-img>
+        <v-img :src="excited" v-if="result === 'EXCITED'"></v-img>
+        <v-img :src="pain" v-if="result === 'PAIN'"></v-img>
+        <v-img :src="shocked" v-if="result === 'SHOCKED'"></v-img>
+        <v-img :src="smile" v-if="result === 'SMILE'"></v-img>
       </v-col>
       <v-col lass="d-flex flex-column mb-6">
         <p class="text-h2 text-center mt-5 mb-5">{{ result }}</p>
@@ -32,11 +32,41 @@ const OnClick = (name: string) => {
           :color="result == 'ANGRY' ? 'yellow' : 'undefined'"
           >ANGRY</v-btn
         >
-        <v-btn :class="'mb-4'" width="100%">CUTE</v-btn>
-        <v-btn :class="'mb-4'" width="100%">EXCITED</v-btn>
-        <v-btn :class="'mb-4'" width="100%">PAIN</v-btn>
-        <v-btn :class="'mb-4'" width="100%">SHOCKED</v-btn>
-        <v-btn :class="'mb-4'" width="100%">SMILE</v-btn>
+        <v-btn
+          :class="'mb-4'"
+          width="100%"
+          @click="OnClick('CUTE')"
+          :color="result == 'CUTE' ? 'yellow' : 'undefined'"
+          >CUTE</v-btn
+        >
+        <v-btn
+          :class="'mb-4'"
+          width="100%"
+          @click="OnClick('EXCITED')"
+          :color="result == 'EXCITED' ? 'yellow' : 'undefined'"
+          >EXCITED</v-btn
+        >
+        <v-btn
+          :class="'mb-4'"
+          width="100%"
+          @click="OnClick('PAIN')"
+          :color="result == 'PAIN' ? 'yellow' : 'undefined'"
+          >PAIN</v-btn
+        >
+        <v-btn
+          :class="'mb-4'"
+          width="100%"
+          @click="OnClick('SHOCKED')"
+          :color="result == 'SHOCKED' ? 'yellow' : 'undefined'"
+          >SHOCKED</v-btn
+        >
+        <v-btn
+          :class="'mb-4'"
+          width="100%"
+          @click="OnClick('SMILE')"
+          :color="result == 'SMILE' ? 'yellow' : 'undefined'"
+          >SMILE</v-btn
+        >
       </v-col>
     </v-row>
   </v-container>
