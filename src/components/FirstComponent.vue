67a6d48618db986b09968c784e547e70a1d11b9f
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-22 00:42:52 +0700

    add btn

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts"></script>
 <template>
   <v-card title="First Component" width="250px" height="150px"></v-card>
+  <v-btn>INCREMENT</v-btn>
 </template>
--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts"></script>
 <template>
   <v-card title="Second Component" width="250px" height="150px"></v-card>
+  <v-btn>DECREASE</v-btn>
 </template>
