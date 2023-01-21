c503cc371156fccac8f051dea98a5bd573663986
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-01-21 19:50:12 +0700

    add function onclick yellow btn add show text

--- src/views/SolveProblem1.vue
+++ src/views/SolveProblem1.vue
@@ -7,21 +7,31 @@ import pain from "../../feeling_imgs/pain.png";
 import shocked from "../../feeling_imgs/shocked.png";
 import smile from "../../feeling_imgs/smile.png";
 const result = ref("");
+const OnClick = (name: string) => {
+  result.value = name;
+};
 </script>
 
 <template>
   <v-container>
     <v-row>
       <v-col>
-        <v-img :src="angry"></v-img>
+        <v-img :src="angry" v-if="result === 'angry'"></v-img>
         <v-img :src="cute"></v-img>
         <v-img :src="excited"></v-img>
         <v-img :src="pain"></v-img>
         <v-img :src="shocked"></v-img>
         <v-img :src="smile"></v-img>
       </v-col>
-      <v-col>
-        <v-btn :class="'mb-4'" width="100%">ANGRY</v-btn>
+      <v-col lass="d-flex flex-column mb-6">
+        <p class="text-h2 text-center mt-5 mb-5">{{ result }}</p>
+        <v-btn
+          :class="'mb-4'"
+          width="100%"
+          @click="OnClick('ANGRY')"
+          :color="result == 'ANGRY' ? 'yellow' : 'undefined'"
+          >ANGRY</v-btn
+        >
         <v-btn :class="'mb-4'" width="100%">CUTE</v-btn>
         <v-btn :class="'mb-4'" width="100%">EXCITED</v-btn>
         <v-btn :class="'mb-4'" width="100%">PAIN</v-btn>
